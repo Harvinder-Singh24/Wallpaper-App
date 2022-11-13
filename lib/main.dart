44bd4bef81c8a37bef937bef4915a15c569a28e6
Harvinder Singh <69596941+Hackersvilla@users.noreply.github.com>
2022-11-13 19:32:01 +0530

    Added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:wallpaper/provider/wallpaper_provider.dart';
-import 'package:wallpaper/screens/splash_page.dart';
+import 'package:wallpaper/provider/main_provider.dart';
+import 'package:wallpaper/screens/emailverify_screen.dart';
+import 'package:wallpaper/screens/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<WallpaperProvider>(
       create: (_) => WallpaperProvider(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
-      ),
+      child: MaterialApp(
+          title: 'Walldeco',
+          theme: ThemeData(
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const SplashScreen()),
     );
   }
 }
